80e1f62149b7c7f40d9f975c2b24249814685486
aswathi555 <106058949+aswathi555@users.noreply.github.com>
2022-12-16 11:01:31 +0530

    Add files via upload

---
+++ journey/tourproject/credentialsapp/urls.py
@@ -0,0 +1,12 @@
+from.import views
+from django.urls import path
+
+from tourproject import settings
+
+urlpatterns = [
+
+    path('register',views.register,name='register'),
+    path('login',views.login,name='login'),
+    path('logout',views.logout,name='logout'),
+    path('about',views.about,name='about'),
+]
